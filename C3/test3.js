b40b8b09ce3538e2f7a484aba46bb9be5f44fc78
RafiIzzaturohman <rafiizzaturohmna06@gmail.com>
2022-08-30 19:26:43 +0700

    Challange

--- C3/test3.js
+++ C3/test3.js
@@ -1,5 +1,28 @@
 function romawi(n) {
+    var roma = {
+        M: 1000,
+        CM: 900,
+        D: 500,
+        CD: 400,
+        C: 100,
+        XC: 90,
+        L: 50,
+        XL: 40,
+        X: 10,
+        IX: 9,
+        V: 5,
+        IV: 4,
+        I: 1
+      };
+      var str = '';
     
+      for (var i of Object.keys(roma)) {
+        var q = Math.floor(n / roma[i]);
+        n -= q * roma[i];
+        str += i.repeat(q);
+      }
+    
+      return str;
 }
 
 console.log("Script Testing untuk Konversi Romawi\n");
