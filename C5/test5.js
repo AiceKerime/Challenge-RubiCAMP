fb5b5e2d3d0ab2ad63164bf81773e47d1c0a4eb4
AiceKerime <rafiizzaturohman06@gmail.com>
2022-09-01 13:08:42 +0700

    Challenge

--- C5/test5.js
+++ C5/test5.js
@@ -1,13 +1,13 @@
 function stringManipulation(word) {
-    if (word.startsWith('a') || word.startsWith('i') || word.startsWith('u') || word.startsWith('e') || word.startsWith('o')) {
+    if (word.startsWith('a') || word.startsWith('i') || word.startsWith('u') || word.startsWith('e') || word.startsWith('o') || word.startsWith('A') || word.startsWith('I') || word.startsWith('U') || word.startsWith('E') || word.startsWith('O')) {
         console.log(word)
     } else {
         console.log(`${word.substr(1)} nyo`.replace(' ', word[0]))
     }
 }
 
-stringManipulation('ayam') // => 'ayam'
-// stringManipulation('iyam') // => 'iyam'
+stringManipulation('ITIK') // => 'ayam'
+stringManipulation('iyam') // => 'iyam'
 // stringManipulation('uyam') // => 'uyam'
 // stringManipulation('eyam') // => 'eyam'
 // stringManipulation('oyam') // => 'oyam'
