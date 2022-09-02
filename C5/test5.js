0fae564f9fb9d56d2cf0e6ebaddfe0c2372f044f
AiceKerime <rafiizzaturohman06@gmail.com>
2022-09-02 09:41:28 +0700

    Challenge

--- C5/test5.js
+++ C5/test5.js
@@ -2,13 +2,11 @@ function stringManipulation(word) {
     if (word.startsWith('a') || word.startsWith('i') || word.startsWith('u') || word.startsWith('e') || word.startsWith('o') || word.startsWith('A') || word.startsWith('I') || word.startsWith('U') || word.startsWith('E') || word.startsWith('O')) {
         console.log(word)
     } else {
-        console.log(`${word.substr(1)} nyo`.replace(' ', word[0]))
+        console.log(`${word.substr(1)}${word[0]}nyo`)
     }
 }
 
+
 stringManipulation('ITIK') // => 'ayam'
-stringManipulation('iyam') // => 'iyam'
-// stringManipulation('uyam') // => 'uyam'
-// stringManipulation('eyam') // => 'eyam'
-// stringManipulation('oyam') // => 'oyam'
-stringManipulation('kucing') // => 'ebekbnyo'
+stringManipulation('ayam') // => 'iyam'
+stringManipulation('bebek') // => 'ebekbnyo'
---
+++ C6/test6.1.js
@@ -0,0 +1,18 @@
+function sentencesManipulation(sentence) {
+    let str = ' '
+    let wordSplit = sentence.split(' ')
+
+    for (let i = 0; i < wordSplit.length; i++) {
+        let word = wordSplit[i]
+        let hurufAwal = wordSplit[i].charAt(0);
+
+        if (word.startsWith('a') || word.startsWith('i') || word.startsWith('u') || word.startsWith('e') || word.startsWith('o') || word.startsWith('A') || word.startsWith('I') || word.startsWith('U') || word.startsWith('E') || word.startsWith('O')) {
+            str += wordSplit[i] + ' '
+        } else {
+            str += wordSplit[i].substr(1) + hurufAwal + 'nyo '
+        }
+    } 
+    console.log(str)
+}
+
+sentencesManipulation('kucing emak makan ikan')
