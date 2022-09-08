79d538606f1f6b6cff52055b2a28b2a11229bb38
AiceKerime <rafiizzaturohman06@gmail.com>
2022-09-08 07:06:50 +0700

    Challenge

---
+++ C11/test11.js
@@ -0,0 +1,47 @@
+// const { Console } = require('console');
+const fs = require('fs');
+const readline = require('readline');
+
+const file = fs.readFileSync('./C11/data.json')
+const data = JSON.parse(file)
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    prompt: 'Tebakanmu > '
+});
+
+console.log("Hey, yo! Selamat datang di uji pengetahuan tentang sejarah bangsa Indonesia!\n")
+
+let count = 0;
+
+console.log(`Pertanyaan : ${data[count].definition}`); // .definition untuk memanggil value dari properti yang bernama definition
+rl.prompt();
+
+rl.on('line', line => {
+    if (count < data.length-1) {
+        if (line.toLowerCase() !== data[count].term) { // .term untuk memanggil properti dari object di data.json yang 
+            console.log('Wkwkwwkwk, Anda jawabanmu kurang tepat!')
+            rl.prompt()
+        } else {
+            count++
+            console.log('Naise, jawabanmu benar!\n')
+            console.log(`Pertanyaan: ${data[count].definition}`)
+            rl.prompt()
+        }
+    } else {
+        if (line.toLowerCase() !== data[count].term) {
+            console.log('Wkwkwwkwk, Anda jawabnmu kurang tepat!')
+            console.log(`Pertanyaan: ${data[count].definition}`)
+            rl.prompt()
+        } else {
+            console.log('Naise, jawabanmu benar!\n')
+            console.log('Yow mantap, Anda menang quiz nya :D')
+            process.exit(0)
+        }
+    }
+  rl.prompt();
+}).on('close', () => {
+  console.log('Dadah xD');
+  process.exit(0);
+});
