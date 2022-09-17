f125efd4b116bd11db4d8ec60a57a19070ba2a1e
AiceKerime <rafiizzaturohman06@gmail.com>
2022-09-17 10:41:38 +0700

    Challenge

---
+++ OOP/C16/test16.js
@@ -0,0 +1,20 @@
+class CarFactory {
+    constructor(merk1, merk2) {
+        this.merk1 = merk1
+        this.merk2 = merk2
+
+        this.cars = []
+    }
+
+    static random(){
+        return Math.floor
+    }
+}
+
+class Car {
+
+}
+
+class Tyre {
+
+}
