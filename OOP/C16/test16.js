bd36f6b9498718a6cb95e9dacaccb6dc86430144
AiceKerime <rafiizzaturohman06@gmail.com>
2022-09-19 10:18:41 +0700

    Challenge

--- OOP/C16/test16.js
+++ OOP/C16/test16.js
@@ -1,73 +1,90 @@
 class CarFactory {
-    constructor(merk1, merk2) {
+    // Menggunakan prorotype method
+    constructor(merk1, merk2, merk3) {
         this.merk1 = merk1
         this.merk2 = merk2
+        this.merk3 = merk3
 
         this.cars = []
     }
 
+    // Menggunakan static method
     static random() {
         return Math.floor(Math.random() * 10) + 1;
     }
 
-    produksi(year) {
-        let A = 0;
+    // Prototype method yang fungsinya untuk menghasilkan jumlah produksi mobil yang kemudian dimasukkan ke variable cars yang ada di constructor
+    produksi(total) {
+        let A = 5;
         for (let i = 0; i < CarFactory.random(); i++) {
-            const car1 = new E30(year)
-
+            const car1 = new E30(total)
             this.cars.push(car1)
             A++;
         }
 
-        let B = 0;
+        let B = 3;
         for (let i = 0; i < CarFactory.random(); i++) {
-            const car2 = new AE86(year);
+            const car2 = new AE86(total);
 
             this.cars.push(car2)
             B++
         }
 
-        console.log(`Pada tahun ${year}, perusahaan ${this.merk1} memproduksi sebanyak ${A} unit mobil.
-Pada tahun yang sama perusahaan ${this.merk2} juga memproduksi mobil sebanyak ${B} unit mobil.`)
+        let C = 7;
+        for (let i = 0; i < CarFactory.random(); i++) {
+            const car3 = new Civic(total);
+
+            this.cars.push(car3)
+            C++
+        }
+
+
+        console.log(`Pada tahun 1990, perusahaan ${this.merk1} memproduksi sebanyak ${A} unit mobil.
+Pada tahun yang sama perusahaan ${this.merk2} juga memproduksi mobil sebanyak ${B} unit mobil.
+Perusahaan ${this.merk3}`)
     }
 
-    static maUUID() {
+    // Salah satu kemampuan dari NodeJS untuk me-generate suatu nomor unik secara universal atau umum (UUID) dengan  menggunakan static method
+    static MyUUID() {
         const { v4: uuidv4 } = require('uuid');
         uuidv4();
 
         return uuidv4()
     }
 
+    // Prototype Method
     garansi(year) {
-        console.log('\nNama Produk :')
-
+        console.log("\nNama Produk:")
         for (let i = 0; i < this.cars.length; i++) {
-            let y = year
+            let y = year;
 
             if (y > (this.cars[i].garansi + this.cars[i].yearz)) {
                 console.log(`
-==========================================================
-                `)
-                console.log(`Merk: ${this.cars[i].brand}
-Model: ${this.cars[i].model}
-Nomor mesin: ${this.cars[i].noMesin}`)
-                console.log(`Waktu garansi: ${this.cars[i].garansi} tahun.
+==========================================================================================
 
-Garansi akan tidak aktif pada tahun ${y}, karena diproduksi tahun ${this.cars[i].yearz}`)
+Merk: ${this.cars[i].brand}
+Model: ${this.cars[i].model} 
+Nomor Mesin: ${this.cars[i].noMesin}
+Dengan waktu garansi ${this.cars[i].garansi} tahun, di tahun ${y} garansi sudah TIDAK AKTIF,
+karena diproduksi pada tahun ${this.cars[i].yearz} dan sudah habis pada tahun ${this.cars[i].garansi + this.cars[i].yearz}.\n`)
             } else {
                 console.log(`
-==========================================================
-                `)
-                console.log(`Merk: ${this.cars[i].brand}
+==========================================================================================
+
+Merk: ${this.cars[i].brand}
 Model: ${this.cars[i].model}
-Nomor mesin: ${this.cars[i].noMesin}`)
-                console.log(`Waktu garansi: ${this.cars[i].garansi} tahun.
-Garansi akan tidak aktif pada tahun ${y}, karena diproduksi tahun ${this.cars[i].yearz}`)
+Nomor Mesin: ${this.cars[i].noMesin}
+Dengan waktu garansi ${this.cars[i].garansi} tahun, di tahun ${y} garansi masih AKTIF, 
+karena diproduksi pada tahun ${this.cars[i].yearz} dan baru akan habis pada tahun ${this.cars[i].garansi + this.cars[i].yearz}.\n`)
             }
+
         }
+
     }
 }
 
+
+// Class yang digunakan untuk pemanggilan properti yang mana properti nya dimasukkan ke dalam constructor, isinya itu berupa spesifikasi dari mobil. Menggunakan property method
 class Car {
     constructor(brand, model, yearz, gr, sit, door, tyre) {
         this.brand = brand;
@@ -77,10 +94,11 @@ class Car {
         this.sit = sit;
         this.door = door;
         this.tyre = tyre;
-        this.noMesin = CarFactory.maUUID();
+        this.noMesin = CarFactory.MyUUID();
     }
 }
 
+// Class yang digunakan untuk pemanggilan properti yang mana properti nya dimasukkan ke dalam constructor, isinya itu berupa informasi spesifikasi ban yang digunakan oleh mobil. Menggunakan property method
 class Ban {
     constructor(size, TyreBrand) {
         this.size = size
@@ -88,19 +106,28 @@ class Ban {
     }
 }
 
+// Inheritance, yang isinya itu adalah untuk menginputkan data mobil berdasarkan dari properti yang ada di class Car
 class E30 extends Car {
     constructor() {
-        super('BMW', 'E30', 1989, 10, 7, 5, new Ban(4, 'Bridgestone'))
+        super('BMW', 'E30', 1989, 5, 5, 5, new Ban(4, 'GT Radial'))
     }
 }
 
+// Inheritance, yang isinya itu adalah untuk menginputkan data mobil berdasarkan dari properti yang ada di class Car
 class AE86 extends Car {
     constructor() {
-        super('Toyota', 'AE86 Trueno', 1984, 10, 7, 5, new Ban(4, 'Dunlop'))
+        super('Toyota', 'AE86 Trueno', 1984, 10, 5, 5, new Ban(4, 'Goodyear'))
     }
 }
 
-let comp = new CarFactory('BMW', 'Toyota')
+// Inheritance atau pewa, yang isinya itu adalah untuk menginputkan data mobil berdasarkan dari properti yang ada di class Car
+class Civic extends Car {
+    constructor() {
+        super('Honda', 'Civic SB3', 1990, 10, 5, 5, new Ban(4, 'Perill'))
+    }
+}
+
+let comp = new CarFactory('BMW', 'Toyota', 'Honda')
 let date = (Math.floor(Math.random() * 15) + 2000)
 comp.produksi(date)
 comp.garansi(date)
