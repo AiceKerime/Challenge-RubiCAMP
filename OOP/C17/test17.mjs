b59c4d20aaf561de39f1e9c185863c99312394f3
AiceKerime <rafiizzaturohman06@gmail.com>
2022-09-20 16:04:11 +0700

    Challenge

---
+++ OOP/C17/test17.mjs
@@ -0,0 +1,46 @@
+export const Pi = 22 / 7;
+
+export default class MesinHitung {
+    constructor() {
+        this.x = 1
+    }
+
+    add(x) {
+        this.x += x;
+        return this
+    }
+
+    substract(x) {
+        this.x -= x;
+        return this
+    }
+
+    multiply(x) {
+        this.x *= this.x;
+        return this
+    }
+
+    divide(x) {
+        this.x /= x;
+        return this
+    }
+
+    square() {
+        this.x = Math.pow(this.x, 2);
+        return this
+    }
+
+    exponent(x) {
+        this.x **= x
+        return this
+    }
+
+    squareRoot() {
+        this.x = Math.sqrt(this.x);
+        return this
+    }
+
+    result() {
+        console.log(this.x)
+    }
+}
